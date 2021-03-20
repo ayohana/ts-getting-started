226239d0dce15f395440cbac3ab3b12a4ae5a687
Adela Darmansyah <v-adelad@microsoft.com>
2021-03-20 13:01:18 -0700

    Add main as a new module

---
+++ main.ts
@@ -0,0 +1,12 @@
+// To run this module, enter the following CLI command:
+// node ./build/main.js
+// Output will be displayed in the console
+
+import { returnGreeting } from './greetings_module.js';             // imports a single function in the module
+import * as allGreetingFunctions from './greetings_module.js';   // imports all exported components in the module
+
+import { returnGreeting as returnGreetingLength } from './greetings-utilities_module.js';
+
+returnGreeting('Hola!')         // Displays 'The message from Greetings_module is Hola!'
+allGreetingFunctions.returnGreeting('Bonjour');  // Displays 'The message from Greetings_module is Bonjour!'
+returnGreetingLength('Ciao!');  // Displays 'The message from GreetingsWithLength_module is Ciao! It is 5 characters long.'
